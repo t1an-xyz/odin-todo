26e92913ea9d8e022820697f9cee1687a4e2ff3a
Theo <62950302+t1an-xyz@users.noreply.github.com>
2025-01-10 14:52:50 -0500

    Use local storage

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import { listProps } from './taskList';
 import { updateSidebar } from './Components/sidebar';
 import renderTaskList from './Components/taskListView';
 
+if (localStorage.getItem('listProps')) {
+    let savedSession = JSON.parse(localStorage.getItem('listProps'));
+    listProps.currentList = savedSession.currentList;
+    listProps.taskLists = savedSession.taskLists;
+}
+
 updateSidebar();
 renderTaskList();
--- src/taskList.js
+++ src/taskList.js
@@ -11,11 +11,13 @@ export default class TaskList {
     addTask(title, description, dueDate, priority) {
         this.tasks[this.nextId] = new Task(this.nextId, title, description, dueDate, priority);
         this.nextId++;
+        localStorage.setItem('listProps', JSON.stringify(listProps));
         return this.nextId - 1;
     }
 
     removeTask(taskId) {
         delete this.tasks[taskId];
+        localStorage.setItem('listProps', JSON.stringify(listProps));
     }
 
     getTask(taskId) {
@@ -24,6 +26,7 @@ export default class TaskList {
 
     editTask(taskId, title, description, dueDate, priority) {
         this.tasks[taskId] = new Task(taskId, title, description, dueDate, priority);
+        localStorage.setItem('listProps', JSON.stringify(listProps));
     }
 }
 
@@ -34,9 +37,11 @@ let listProps = {
     taskLists: [new TaskList(0, 'My List')],
     removeList: (listId) => {
         listProps.taskLists = listProps.taskLists.filter(list => list.id !== listId);
+        localStorage.setItem('listProps', JSON.stringify(listProps));
     },
     addList: (listName) => {
         listProps.taskLists.push(new TaskList(nextId++, listName));
+        localStorage.setItem('listProps', JSON.stringify(listProps));
     }
 }
 
